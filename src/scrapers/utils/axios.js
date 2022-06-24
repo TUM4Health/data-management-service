a3bef9f60574bc7ee0405e46adf47c275066cc11
Philipp Zagar <philipp.zagar@gmail.com>
2022-06-24 08:27:53 +0200

    Adapt to Bennis comments

--- src/scrapers/utils/axios.js
+++ src/scrapers/utils/axios.js
@@ -3,20 +3,24 @@ const axiosRetry = require('axios-retry');
 const http = require('http');
 
 const MAX_REQUESTS_COUNT = 3; // Max number of retries, set to 3 as ZHS website is not stable
-const INTERVAL_MS = 100;
+const INTERVAL_MS = 100;	// Cooldown interval until a new request is fired
+const TIMOUT_MS = 8000;		// Maximum timeinterval until a timeout error is thrown
+const RETRY_DELAY_TIMEOUT = 15000;		// Time period waited until a timeout occures
+const RETRY_DELAY_STANDARD = 2000;		// Time period in case of a normal error (so no timeout)
+const MAXIMUM_RETRIES = 10;		// Maximum amout of retries in case of error
 let PENDING_REQUESTS = 0;
 
 // create new axios instance
-const instance = axios.create({
+const axiosInstance = axios.create({
 	baseURL: '/',
-	timeout: 8000,
+	timeout: TIMOUT_MS,
 	httpAgent: new http.Agent({ keepAlive: true }),
 });
 
 /**
  * Axios Request Interceptor
  */
-instance.interceptors.request.use((config) => new Promise((resolve, reject) => {
+axiosInstance.interceptors.request.use((config) => new Promise((resolve, reject) => {
 	const interval = setInterval(() => {
 		if (PENDING_REQUESTS < MAX_REQUESTS_COUNT) {
 			PENDING_REQUESTS++;
@@ -29,7 +33,7 @@ instance.interceptors.request.use((config) => new Promise((resolve, reject) => {
 /**
  * Axios Response Interceptor
  */
-instance.interceptors.response.use((response) => {
+axiosInstance.interceptors.response.use((response) => {
 	PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
 	return Promise.resolve(response);
 }, (error) => {
@@ -40,16 +44,16 @@ instance.interceptors.response.use((response) => {
 /**
  * Axios Retry Interceptor
  */
-axiosRetry(instance, {
-	retries: 10,
+axiosRetry(axiosInstance, {
+	retries: MAXIMUM_RETRIES,
 	retryDelay: (retryCount, error) => {
-		const retryDelay = (error.message === 'ETIMEDOUT') ? 15000 : (2000 * retryCount);
+		const retryDelay = (error.message === 'ETIMEDOUT') ? RETRY_DELAY_TIMEOUT : (RETRY_DELAY_STANDARD * retryCount);
 		console.log(`Retry attempt: ${retryCount}`);
 		console.log(`Retry delay: ${retryDelay} ms`);
-		return retryDelay; // time interval between retries
+		return retryDelay; 	// time interval between retries
 	},
 	retryCondition: (error) => axiosRetry.isNetworkOrIdempotentRequestError(error)
-            || error.code === 'ECONNABORTED',
+		|| error.code === 'ECONNABORTED',
 });
 
-module.exports = instance;
+module.exports = axiosInstance;
